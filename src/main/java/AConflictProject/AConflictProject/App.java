0887f473fdc8fefb9fdeb0b746fa55a2beb66c72
ERUMALLA RAMESH <107615648+rameshtony@users.noreply.github.com>
2022-06-23 12:51:29 +0530

    Update App.java

--- src/main/java/AConflictProject/AConflictProject/App.java
+++ src/main/java/AConflictProject/AConflictProject/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Learning conflict from Github..this line is added from github!" );
     }
 }
